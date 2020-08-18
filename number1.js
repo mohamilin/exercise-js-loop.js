399bc5a684e9d5acbf984c7d10dc76bdb06813b4
mohamilin <amilinmoh99@gmail.com>
2020-08-18 12:57:42 +0700

    solved number 1 dom html

--- number1.js
+++ number1.js
@@ -1,23 +1,15 @@
 /*
 CHALLENGE : 
-Buat conditional IF .. ELSE IF yang menggambarkan divisi dan tugas divisi pada STARTUP.
-        Contoh: Jika saya sebagai HR maka tugas saya adalah melakukan rekrutmen untuk calon pegawai baru
+Kita sudah mengetahui bahwa banyaknya user maksimal adalah 100.
+    Buat sebuah program yang menampilkan teks ‘User ke - 1 … User ke - 100’ pada setiap baris di halaman HTML.
+    Lakukan FOR LOOP pada Javascript.
 
 */
 
 // SOLVE :
-
-
-let division = "marketing";
-
-if(jobdesk === "HR" ) {
-    console.log("Rekrutmen untuk calon pegawai baru");
-} else if(division === "marketing") {
-    console.log("Memasarkan Produk");
-} else if(division === "manager") {
-    console.log("Mengatur Team");
-} else if(division === "front-end") {
-    console.log("Mengembangkan aplikasi");
-} else {
-    console.log("Menerima Laporan dari seluruh bagian")
+let user = 0;
+for(let user = 1; user <= 100; user++) {
+    document.write("<br>");
+    document.write("User ke - " + user);
 }
+
